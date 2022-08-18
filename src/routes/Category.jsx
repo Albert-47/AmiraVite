14b1eb93091319f2bcb9c3a73e0795fa3a7b8396
Albert <albertquevedo35@gmail.com>
2022-08-18 16:22:34 -0500

    added lazy loading property to images

--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import {
   Card,
   CardContent,
@@ -12,24 +12,24 @@ import {
   InputLabel,
   Select,
   MenuItem,
-  Box,
-} from "@mui/material";
-import "../styles.js";
+  Box
+} from '@mui/material';
+import '../styles.js';
 
 const Categories = () => {
   const [data, setData] = useState([]); //Products to be shown
-  const [filter, setFilter] = useState(""); //Filter option selected by the user
+  const [filter, setFilter] = useState(''); //Filter option selected by the user
 
-  const serverURL = "https://amiraserver.herokuapp.com"; //Server URL
+  const serverURL = 'https://amiraserver.herokuapp.com'; //Server URL
   const hasItem = data[0]; //A confirmation of the received data in the request to the server
 
-  const { category } = useParams("category");
+  const { category } = useParams('category');
 
   //Function to filter out data
 
   const filterData = (param, array) => {
     let newData = [];
-    if (param === "Niños") {
+    if (param === 'Niños') {
       newData = array.filter((item) => item.age === param);
     } else {
       newData = array.filter((item) => item.gender === param);
@@ -45,8 +45,8 @@ const Categories = () => {
       const untouchedData = res.data;
       setData(untouchedData);
 
-      if (filter !== "") {
-        console.log("se ejecuta filter");
+      if (filter !== '') {
+        console.log('se ejecuta filter');
         filterData(filter, untouchedData);
       }
     };
@@ -57,78 +57,79 @@ const Categories = () => {
     <>
       <Box
         sx={{
-          width: { xs: "90%" },
-          margin: { xs: "20px 20px", md: "20px 50px" },
+          width: { xs: '90%' },
+          margin: { xs: '20px 20px', md: '20px 50px' }
         }}
       >
-        <FormControl fullWidth color="warning">
+        <FormControl fullWidth color='warning'>
           <InputLabel>Filtrar por...</InputLabel>
           <Select value={filter} onChange={(e) => setFilter(e.target.value)}>
-            <MenuItem value="Niños">Niños</MenuItem>
-            <MenuItem value="Caballero">Caballero</MenuItem>
-            <MenuItem value="Dama">Dama</MenuItem>
-            <MenuItem value="Unisex">Unisex</MenuItem>
+            <MenuItem value='Niños'>Niños</MenuItem>
+            <MenuItem value='Caballero'>Caballero</MenuItem>
+            <MenuItem value='Dama'>Dama</MenuItem>
+            <MenuItem value='Unisex'>Unisex</MenuItem>
           </Select>
         </FormControl>
       </Box>
-      <Grid container justifyContent="center" spacing={2}>
+      <Grid container justifyContent='center' spacing={2}>
         {data.map((item) => (
           <Grid
             item
             key={item.name}
-            className="animate__animated animate__fadeIn animate__slow"
+            className='animate__animated animate__fadeIn animate__slow'
           >
             <Card
               sx={{
                 maxWidth: 345,
                 mt: 2,
-                minWidth: { xs: "100px", sm: "267px" },
+                minWidth: { xs: '100px', sm: '267px' }
               }}
             >
               <CardMedia
-                component="img"
+                component='img'
                 alt={item.name}
-                height="300"
+                height='300'
                 image={item.img}
                 sx={{ flexGrow: 1, flexShrink: 1 }}
+                loading='lazy'
               />
-              <CardContent sx={{ textAlign: "center" }}>
-                <Typography variant="h5">{item.name}</Typography>
+              <CardContent sx={{ textAlign: 'center' }}>
+                <Typography variant='h5'>{item.name}</Typography>
                 <Typography
                   gutterBottom
-                  variant="body1"
-                  color="orange"
-                  sx={{ display: "inline" }}
+                  variant='body1'
+                  color='orange'
+                  sx={{ display: 'inline' }}
                 >
                   <i>{item.brand}, </i>
                 </Typography>
                 <Typography
                   gutterBottom
-                  variant="body1"
+                  variant='body1'
                   color={`${
-                    item.gender === "Caballero" ? "secondary" : "purple"
+                    item.gender === 'Caballero' ? 'secondary' : 'purple'
                   }`}
-                  sx={{ display: "inline" }}
+                  sx={{ display: 'inline' }}
                 >
                   {item.gender}
                 </Typography>
-                {item.age === "Niños" && (
+                {item.age === 'Niños' && (
                   <Typography
                     gutterBottom
-                    variant="body1"
+                    variant='body1'
                     sx={{
-                      border: "1px solid darkGreen",
-                      bgcolor: "#20a448",
-                      borderRadius: "2px",
+                      border: '1px solid darkGreen',
+                      bgcolor: '#20a448',
+                      borderRadius: '2px'
                     }}
                   >
                     {item.age}
                   </Typography>
                 )}
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant='body2' color='text.secondary'>
                   Precio al Mayor: {item.bulkPrice} <br />
                   Precio al Detal: {item.individualPrice} <br />
-                  {category == ("Morrales" || "Carteras")
+                  {category == ('Morrales' || 'Carteras')
                     ? null
                     : `Tallas: ${item.sizes}`}
                 </Typography>
@@ -142,25 +143,25 @@ const Categories = () => {
     <>
       <Box
         sx={{
-          width: { xs: "90%" },
-          margin: { xs: "20px 20px", md: "20px 50px" },
+          width: { xs: '90%' },
+          margin: { xs: '20px 20px', md: '20px 50px' }
         }}
       >
-        <FormControl fullWidth color="warning">
+        <FormControl fullWidth color='warning'>
           <InputLabel>Filtrar por...</InputLabel>
           <Select value={filter} onChange={(e) => setFilter(e.target.value)}>
-            <MenuItem value="Niños">Niños</MenuItem>
-            <MenuItem value="Caballero">Caballero</MenuItem>
-            <MenuItem value="Dama">Dama</MenuItem>
-            <MenuItem value="Unisex">Unisex</MenuItem>
+            <MenuItem value='Niños'>Niños</MenuItem>
+            <MenuItem value='Caballero'>Caballero</MenuItem>
+            <MenuItem value='Dama'>Dama</MenuItem>
+            <MenuItem value='Unisex'>Unisex</MenuItem>
           </Select>
         </FormControl>
       </Box>
       <Typography
         sx={{
           fontSize: { xs: 30, sm: 50 },
-          textAlign: "center",
-          margin: { sm: "25% 25%", xs: "80% 15%" },
+          textAlign: 'center',
+          margin: { sm: '25% 25%', xs: '80% 15%' }
         }}
       >
         Próximamente...
